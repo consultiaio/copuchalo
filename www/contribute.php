fe89d6c529e3967e695eb22721ac0a5dbe7f0eb6
Fermin Molina <fmolina@gmail.com>
2018-10-27 02:30:52 +0000

    Fixed contribute page

--- www/contribute.php
+++ www/contribute.php
@@ -3,7 +3,7 @@
 include('config.php');
 include(mnminclude.'html1.php');
 
-do_header(_('colabora con mediatize"'), '', false, false, '', false, false);
+do_header(_('colabora con mediatize'), '', false, false, '', false, false);
 
 echo '<div id="singlewrap" class="col-sm-10">';
 echo '<div class="topheading th-no-margin"><h2>Colabora con mediatize.info</h2></div>';
